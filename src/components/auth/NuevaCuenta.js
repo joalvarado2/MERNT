964a1aaffa4f221cc90944f71f2623331afbbf28
joalvarado2 <jonathanalva92@gmail.com>
2021-07-06 20:24:07 -0500

    iniciando MERNT creand login y inicio de sesion

---
+++ src/components/auth/NuevaCuenta.js
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const NuevaCuenta = () => {
+  // State para iniciar sesion
+  const [usuario, setUsuario] = useState({
+    nombre: "",
+    email: "",
+    password: "",
+    confirmar: ""
+  });
+
+  // extraer de usuario
+  const { nombre, email, password, confirmar } = usuario;
+
+  const onChange = (e) => {
+      setUsuario({
+          ...usuario,
+          [e.target.name] : e.target.value
+      })
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    // validacion de campos vacios
+
+    // contraseña minimo 6 caracteres
+
+    // campo contraseña iguales
+
+    // pasar a la accion
+  }
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Obtener una cuenta</h1>
+
+        <form onSubmit={onSubmit}>
+          <div className="campo-form">
+            <label htmlFor="nombre">Nombre</label>
+            <input
+              type="text"
+              id="nombre"
+              name="nombre"
+              placeholder="Tu Nombre"
+              value={nombre}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Tu Email"
+              value={email}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Tu Password"
+              value={password}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <label htmlFor="confirmar">Confirmar Password</label>
+            <input
+              type="password"
+              id="confirmar"
+              name="confirmar"
+              placeholder="Repite tu Password"
+              value={confirmar}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Registrarme"
+            />
+          </div>
+        </form>
+
+        <Link to={"/"} className="enlace-cuenta">
+          Volver a Iniciar Sesión
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NuevaCuenta;
